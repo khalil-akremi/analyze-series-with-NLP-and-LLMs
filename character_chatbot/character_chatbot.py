3774ffa0813ce00566aa2224733a5a00d946a625
Khalil Akremi <khalilakremi0@gmail.com>
2025-08-23 19:24:10 +0100

    character chatbot newwwww

--- character_chatbot/character_chatbot.py
+++ character_chatbot/character_chatbot.py
@@ -125,7 +125,7 @@ class CharacterChatBot():
             lora_dropout=lora_dropout,
             r=lora_r,
             bias="none",
-            task_type="CASUAL_LM"
+            task_type="CAUSAL_LM"
         )
 
         training_arguments = SFTConfig(
